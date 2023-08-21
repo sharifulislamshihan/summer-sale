38496e181a34c9f4695ab3e61c6fd4d43460d442
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-08-21 18:41:55 +0600

    cupon added

---
+++ utilities.js
@@ -0,0 +1,73 @@
+let total = 0;
+let discountPrice = 0;
+let discountAmount = 0;
+
+function checkInput(mytext, value, mybtn) {
+    // Check if the input value meets your condition (e.g., minimum length)
+    const textInput = document.getElementById(mytext);    
+    const myButton = document.getElementById(mybtn);    
+
+    if (textInput.value.length >= value) {
+      // Enable the button
+      myButton.disabled = false;
+    } else {
+      // Disable the button
+      myButton.disabled = true;
+    }
+  }
+
+
+function handleClickBtn(target){
+
+    // making list of the products
+    const productListContainer = document.getElementById('product-list');
+    const itemName = target.childNodes[3].childNodes[3].innerText;
+    const list = document.createElement('li');
+    list.innerText = itemName;
+    productListContainer.appendChild(list);
+
+    // calculating total price
+    const priceString = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    const price = parseFloat(priceString);
+    total = parseFloat(total) + parseFloat(price);
+    const priceTotal = total.toFixed(2);
+    
+    // make purchase button enable
+    const purchaseBtn = document.getElementById('purchase-btn')
+    if(priceTotal>0){
+        purchaseBtn.disabled = false;
+    }
+    else{
+        purchaseBtn.disabled = true;
+    }
+
+    // making cupon button enable
+    const cuponBtn = document.getElementById('cupon-btn')
+    if(priceTotal>= 200){
+        cuponBtn.disabled = false;
+    }
+    else{
+        cuponBtn.disabled = true;
+    }
+
+    document.getElementById('total-price').innerText = priceTotal;
+    const grandTotal = parseFloat(priceTotal).toFixed(2);
+    document.getElementById('grand-total').innerText = grandTotal;
+
+    document.getElementById('cupon-btn').addEventListener('click', function(){
+        const cuponInputTextString = document.getElementById('cupon-input');
+        const cuponInputText = cuponInputTextString.value;
+        // calculating discount price
+        //20% off
+        const discountAmount = parseFloat(total * (20/100)).toFixed(2);
+        if(cuponInputText === "SELL200"){
+            document.getElementById('discount-price').innerText = discountAmount;
+            const grandTotal = parseFloat(total) - parseFloat(discountAmount);
+                // grand total
+            document.getElementById('grand-total').innerText = grandTotal;
+            cuponInputTextString.value = '';
+        }
+    })
+    
+    
+}
